879fb11356c6b4b3b337ed3527dfc183bcee3613
Kyle <kylerhoads45@gmail.com>
2020-11-16 23:06:41 -0800

    Added unit tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,45 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+TEST(PracticeTest, is_not_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("aba");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, empty_string_palidrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, simple_sort_desc)
+{
+    Practice obj;
+    bool actual = obj.sortDescending(1, 2, 3);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, simple_dont_sort_desc)
+{
+    Practice obj;
+    bool actual = obj.sortDescending(3, 2, 1);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, simple_no_order_sort_desc)
+{
+    Practice obj;
+    bool actual = obj.sortDescending(1, 3, 2);
+    ASSERT_TRUE(actual);
+}
+
